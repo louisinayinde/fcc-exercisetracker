e57ee8b9fa0bc76475c3cb0b1b0efc02ea8a6a98
louisinayinde <louisinayinde@gmail.com>
2024-04-05 08:44:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, ()=> {
-  console.log('Connected to DB succesfully')
-})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = mongoose.Schema({
   username: {
