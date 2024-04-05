1661c7e59105ccd419fed0bfc147c0de66d80c90
louisinayinde <louisinayinde@gmail.com>
2024-04-05 07:51:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -70,16 +70,38 @@ app.post('/api/users/:_id/exercises', (req, res) => {
 
   let newExercise = new exerciseModel(exerciseObj);
 
-  userModel.findById(userId, (err, userFound) => {
-    if (err) console.log(err);
+  try{
 
+  }catch{
+
+  }
+
+try {
+    // Tente de trouver l'utilisateur par son ID
+    const userFound = userModel.findById(userId);
+    if (!userFound) {
+      // Gérer le cas où l'utilisateur n'est pas trouvé
+      console.log("User not found");
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    // Si l'utilisateur est trouvé, sauvegarder l'exercice
     newExercise.save();
+
+    // Envoyer une réponse JSON avec les informations pertinentes
     res.json({
-      _id: userFound._id, username: userFound.username,
-      description: newExercise.description, duration: newExercise.duration,
+      _id: userFound._id,
+      username: userFound.username,
+      description: newExercise.description,
+      duration: newExercise.duration,
       date: new Date(newExercise.date).toDateString()
-    })
-  })
+    });
+  } catch (err) {
+    // Gérer les erreurs éventuelles lors de la recherche ou de la sauvegarde
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
 })
 
 app.get('/api/users/:_id/logs', (req, res) => {
@@ -92,48 +114,48 @@ app.get('/api/users/:_id/logs', (req, res) => {
   // If limit param exists set it to an integer
   limitParam = limitParam ? parseInt(limitParam): limitParam
 
-  userModel.findById(userId, (err, userFound) => {
-    if (err) return console.log(err);
+  try {
+    const userFound = userModel.findById(userId);
     console.log(userFound);
-    
-      let queryObj = {
-        userId: userId
-      };
-      // If we have a date add date params to the query
-      if (fromParam || toParam){
-    
-          queryObj.date = {}
-          if (fromParam){
-            queryObj.date['$gte'] = fromParam;
-          }
-          if (toParam){
-            queryObj.date['$lte'] = toParam;
-          }
-        }
 
-    
-    exerciseModel.find(queryObj).limit(limitParam).exec((err, exercises) => {
-      if (err) return console.log(err);
-  
-      let resObj = 
-        {_id: userFound._id,
-         username: userFound.username
-        }
-  
-      exercises = exercises.map((x) => {
-        return {
-          description: x.description,
-          duration: x.duration,
-          date: new Date(x.date).toDateString()
-        }
-      })
-      resObj.log = exercises;
-      resObj.count = exercises.length;
-      
-      res.json(resObj);
-    })
-    
-  })
+    let queryObj = {
+      userId: userId
+    };
+
+    if (fromParam || toParam) {
+      queryObj.date = {};
+      if (fromParam) {
+        queryObj.date['$gte'] = fromParam;
+      }
+      if (toParam) {
+        queryObj.date['$lte'] = toParam;
+      }
+    }
+
+    // Utilisez await avec exec() et supprimez le callback
+    let exercises = exerciseModel.find(queryObj).limit(limitParam).exec();
+
+    let resObj = {
+      _id: userFound._id,
+      username: userFound.username,
+      log: exercises.map(x => ({
+        description: x.description,
+        duration: x.duration,
+        date: new Date(x.date).toDateString()
+      })),
+      count: exercises.length
+    };
+
+    // Ici, vous devez retourner resObj ou le gérer comme nécessaire
+    // Par exemple, vous pouvez le retourner si cette fonction est appelée ailleurs
+    return resObj;
+
+  } catch (err) {
+    console.log(err);
+    // Gérez l'erreur comme nécessaire, par exemple, en retournant un message d'erreur
+    // Ou en lançant à nouveau l'erreur selon le contexte d'utilisation
+    throw err; // ou return { error: "Un problème est survenu" };
+  }
 })
 
 
